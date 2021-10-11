4f07e38c27b9b594196b4f92267220aa5c5efac4
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-10-11 15:18:41 +1300

    immigration of scitype() overloading from ScientificTypes 2.3.1

---
+++ src/scitypes.jl
@@ -0,0 +1,34 @@
+const STB = ScientificTypesBase
+
+# aliases not exported:
+const PlainNGram{N}  = NTuple{N,<:AbstractString}
+const TaggedNGram{N} = NTuple{N,<:CL.TaggedWord}
+
+# This can be made less of a hack once ScientificTypes #155 is sorted.
+
+type2scitype(T::Type) = STB.Scitype(T, DefaultConvention())
+type2scitype(::Type{<:AbstractVector{T}}) where T =
+    AbstractVector{type2scitype(T)}
+type2scitype(::NTuple{N,T}) where {N,T} = NTuple{type2scitype{T}}
+
+STB.scitype(::CL.TaggedWord, ::DefaultConvention) = Annotated{Textual}
+STB.scitype(::CL.Document{<:AbstractVector{T}}, ::DefaultConvention) where T =
+    Annotated{AbstractVector{type2scitype(T)}}
+STB.scitype(::AbstractDict{<:AbstractString,<:Integer},
+           ::DefaultConvention) = Multiset{Textual}
+STB.scitype(::AbstractDict{<:CL.TaggedWord,<:Integer},
+           ::DefaultConvention) = Multiset{Annotated{Textual}}
+STB.scitype(::AbstractDict{<:Union{CL.TaggedWord,AbstractString},<:Integer},
+           ::DefaultConvention) =
+               Multiset{Union{Textual,Annotated{Textual}}}
+STB.scitype(::AbstractDict{<:PlainNGram{N}}) where N =
+    Multiset{NTuple{N,Textual}}
+STB.scitype(::AbstractDict{<:TaggedNGram{N}}) where N =
+    Multiset{NTuple{N,Annotated{Textual}}}
+STB.scitype(::AbstractDict{<:PlainNGram}) =
+    Multiset{NTuple{<:Any,Textual}}
+STB.scitype(::AbstractDict{<:TaggedNGram}) =
+    Multiset{NTuple{<:Any,Annotated{Textual}}}
+
+STB.Scitype(::Type{<:CL.TaggedWord}, ::DefaultConvention) =
+    Annotated{Textual}
