9c67aed2a41d10df2f1b38652365210e6219131a
josephsdavid <josephsd@smu.edu>
2022-08-29 14:15:03 -0500

    update

--- src/bm25_transformer.jl
+++ src/bm25_transformer.jl
@@ -1,37 +1,117 @@
 """
-    BM25Transformer()
+$(MMI.doc_header(BM25Transformer))
 
-Convert a collection of raw documents to a matrix using the Okapi BM25 document-word statistic.
+`BM25Transformer`: Convert a collection of raw documents to a matrix using the Okapi BM25
+document-word statistic. BM25 is an approach similar to that of TF-IDF in terms of
+representing documents in a vector space.  The BM25 scoring function uses both term
+frequency (TF) and inverse document frequency (IDF) so that, for each term in a document,
+its relative concentration in the document is scored (like TF-IDF). However, BM25 improves
+upon TF-IDF by incorporating probability - particularly, the probability that a user will
+consider a search result relevant based on the terms in the search query and those in each
+document.
 
-BM25 is an approach similar to that of TF-IDF in terms of representing documents in a vector
-space.  The BM25 scoring function uses both term frequency (TF) and inverse document frequency 
-(IDF) so that, for each term in a document, its relative concentration in the document is
-scored (like TF-IDF). However, BM25 improves upon TF-IDF by incorporating probability - particularly,
-the probability that a user will consider a search result relevant based on the terms in the search query
-and those in each document.
 
-The parameters `max_doc_freq`, `min_doc_freq`, and `smooth_idf` all work identically to those in the
-`TfidfTransformer`. BM25 introduces two additional parameters:
 
-`κ` is the term frequency saturation characteristic. Higher values represent slower saturation. What 
-we mean by saturation is the degree to which a term occuring extra times adds to the overall score. This defaults
-to 2.
+# Training data
 
-`β` is a parameter, bound between 0 and 1, that amplifies the particular document length compared to the average length.
-The bigger β is, the more document length is amplified in terms of the overall score. The default value is 0.75.
 
-For more explanations, please see:
-- http://ethen8181.github.io/machine-learning/search/bm25_intro.html
-- https://en.wikipedia.org/wiki/Okapi_BM25
-- https://nlp.stanford.edu/IR-book/html/htmledition/okapi-bm25-a-non-binary-model-1.html
+In MLJ or MLJBase, bind an instance `model` to data with
 
-The parameters `max_doc_freq` and `min_doc_freq` restrict the vocabulary
-that the transformer will consider. `max_doc_freq` indicates that terms in only
-up to the specified percentage of documents will be considered. For example, if
-`max_doc_freq` is set to 0.9, terms that are in more than 90% of documents
-will be removed. Similarly, the `min_doc_freq` parameter restricts terms in the
-other direction. A value of 0.01 means that only terms that are at least in 1% of
-documents will be included.
+mach = machine(model, X)
+
+Where
+
+- `X`: is any matrix of input features whose items are of scitype
+  `ScientificTypesBase.Textual`, `ScientificTypesBase.{Multiset{<:ScientificNGram}}`, or
+  `ScientificTypesBase.Multiset{.ScientificTypesBase.Textual}`; check the scitype with
+  `schema(X)`
+
+Train the machine using `fit!(mach, rows=...)`.
+
+
+# Hyper-parameters
+
+
+- `max_doc_freq=1.0`: Restricts the vocabulary that the transformer will consider.
+  Terms that occur in `> max_doc_freq` documents will not be considered by the
+  transformer. For example, if `max_doc_freq` is set to 0.9, terms that are in more than
+  90% of the documents will be removed.
+- `min_doc_freq=0.0`: Restricts the vocabulary that the transformer will consider.
+  Terms that occur in `< max_doc_freq` documents will not be considered by the
+  transformer. A value of 0.01 means that only terms that are at least in 1% of the
+  documents will be included.
+- `κ=2`: The term frequency saturation characteristic. Higher values represent slower
+  saturation. What we mean by saturation is the degree to which a term occurring extra
+  times adds to the overall score.
+- `β=0.075`: Amplifies the particular document length compared to the average length. The
+  bigger β is, the more document length is amplified in terms of the overall score. The
+  default value is 0.75, and the bounds are restricted between 0 and 1.
+- `smooth_idf=true`: Assuming `smooth_idf` is false, IDF is calculated using the equation
+  `idf(t) = log [ n / df(t) ] + 1`, with term `d`, `n` documents, and document frequency
+  `df(t)`. The `1` term outside of the logarithm has the effect that terms with zero idf
+  (i.e. they occur in all documents) will not be entirely ignored. If `smooth_idf` is
+  true, another `1` term is added, giving: `idf(t) = log [ (1 + n) / (1 + df(t)) ] + 1`.
+  These `1`'s have the same affect as adding an extra document which contains every term
+  in the collection exactly once, preventing division by 0.
+
+
+# Operations
+
+
+- `transform(mach, Xnew)`: Return a transformed matrix of type
+  `ScientificTypesBase.Continuous` given new features `Xnew`.
+
+
+# Fitted parameters
+
+
+The fields of `fitted_params(mach)` are:
+
+- `vocab`: A vector containing the string used in the transformer's vocabulary.
+- `idf_vector`: The transformer's calculated IDF vector.
+- `mean_words_in_docs`: The mean number of words in each document.
+
+
+# Examples
+
+
+`BM25Transformer` accepts a variety of inputs. In the example below, we use simple
+tokenized documents:
+
+```julia
+using MLJ, MLJText, TextAnalysis
+
+docs = ["Hi my name is Sam.", "How are you today?"]
+bm25_transformer = BM25Transformer()
+mach = machine(bm25_transformer, tokenize.(docs))
+MLJ.fit!(mach)
+
+fitted_params(mach)
+
+bm25_mat = transform(mach, tokenize.(docs))
+```
+
+We can also use the `TextAnalysis` package to implement funcionality similar to SciKit
+Learn's N-grams:
+
+```julia
+using MLJ, MLJText, TextAnalysis
+
+docs = ["Hi my name is Sam.", "How are you today?"]
+corpus = Corpus(NGramDocument.(docs, 1, 2))
+ngram_docs = ngrams.(corpus)
+
+bm25_transformer = BM25Transformer()
+mach = machine(bm25_transformer, ngram_docs)
+MLJ.fit!(mach)
+fitted_params(mach)
+
+tfidf_mat = transform(mach, ngram_docs)
+```
+
+See also
+[`GaussianNBClassifier`](@ref)
+
 """
 mutable struct BM25Transformer <: AbstractTextTransformer
     max_doc_freq::Float64
@@ -41,13 +121,13 @@ mutable struct BM25Transformer <: AbstractTextTransformer
     smooth_idf::Bool
 end
 
-function BM25Transformer(; 
+function BM25Transformer(;
     max_doc_freq::Float64 = 1.0,
     min_doc_freq::Float64 = 0.0,
     κ::Int=2,
     β::Float64=0.75,
     smooth_idf::Bool = true
-    )    
+    )
     transformer = BM25Transformer(max_doc_freq, min_doc_freq, κ, β, smooth_idf)
     message = MMI.clean!(transformer)
     isempty(message) || @warn message
@@ -103,14 +183,14 @@ function build_bm25!(doc_term_mat::SparseMatrixCSC{T},
     return bm25
 end
 
-function _transform(transformer::BM25Transformer, 
+function _transform(transformer::BM25Transformer,
                     result::BMI25TransformerResult,
                     v::Corpus)
     doc_terms = build_dtm(v, result.vocab)
     bm25 = similar(doc_terms.dtm, eltype(result.idf_vector))
     build_bm25!(doc_terms.dtm, bm25, result.idf_vector, result.mean_words_in_docs; κ=transformer.κ, β=transformer.β)
 
-    # here we return the `adjoint` of our sparse matrix to conform to 
+    # here we return the `adjoint` of our sparse matrix to conform to
     # the `n x p` dimensions throughout MLJ
     return adjoint(bm25)
 end
--- src/tfidf_transformer.jl
+++ src/tfidf_transformer.jl
@@ -1,46 +1,111 @@
 """
-    TfidfTransformer()
+$(MMI.doc_header(TfidfTransformer))
 
-The following is taken largely from scikit-learn's documentation:
-https://github.com/scikit-learn/scikit-learn/blob/main/sklearn/feature_extraction/text.py
 
-Convert a collection of raw documents to a matrix of TF-IDF features.
+`TfidfTransformer`: Convert a collection of raw documents to a matrix of TF-IDF features.
+"TF" means term-frequency while "TF-IDF" means term-frequency times inverse
+document-frequency.  This is a common term weighting scheme in information retrieval, that
+has also found good use in document classification. The goal of using TF-IDF instead of the
+raw frequencies of occurrence of a token in a given document is to scale down the impact of
+tokens that occur very frequently in a given corpus and that are hence empirically less
+informative than features that occur in a small fraction of the training corpus.The formula
+that is used to compute the TF-IDF for a term `t` of a document `d` in a document set is
+`tf_idf(t, d) = tf(t, d) * idf(t)`.
 
-"TF" means term-frequency while "TF-IDF" means term-frequency times
-inverse document-frequency.  This is a common term weighting scheme in
-information retrieval, that has also found good use in document
-classification.
 
-The goal of using TF-IDF instead of the raw frequencies of occurrence
-of a token in a given document is to scale down the impact of tokens
-that occur very frequently in a given corpus and that are hence
-empirically less informative than features that occur in a small
-fraction of the training corpus.
+# Training data
 
-The formula that is used to compute the TF-IDF for a term `t` of a
-document `d` in a document set is `tf_idf(t, d) = tf(t, d) *
-idf(t)`. Assuming `smooth_idf=false`, `idf(t) = log [ n / df(t) ] + 1`
-where `n` is the total number of documents in the document set and
-`df(t)` is the document frequency of `t`. The document frequency is
-the number of documents in the document set that contain the term
-`t`. The effect of adding “1” to the idf in the equation above is that
-terms with zero idf, i.e., terms that occur in all documents in a
-training set, will not be entirely ignored. (Note that the idf formula
-above differs from that appearing in standard texts, `idf(t) = log [ n
-/ (df(t) + 1) ])`.
 
-If `smooth_idf=true` (the default), the constant “1” is added to the
-numerator and denominator of the idf as if an extra document was seen
-containing every term in the collection exactly once, which prevents
-zero divisions: `idf(t) = log [ (1 + n) / (1 + df(t)) ] + 1`.
+In MLJ or MLJBase, bind an instance `model` to data with
 
-The parameters `max_doc_freq` and `min_doc_freq` restrict the vocabulary
-that the transformer will consider. `max_doc_freq` indicates that terms in only
-up to the specified percentage of documents will be considered. For example, if
-`max_doc_freq` is set to 0.9, terms that are in more than 90% of documents
-will be removed. Similarly, the `min_doc_freq` parameter restricts terms in the
-other direction. A value of 0.01 means that only terms that are at least in 1% of
-documents will be included.
+mach = machine(model, X)
+
+Where
+
+- `X`: is any matrix of input features whose items are of scitype
+  `ScientificTypesBase.Textual`, `ScientificTypesBase.{Multiset{<:ScientificNGram}}`, or
+  `ScientificTypesBase.Multiset{.ScientificTypesBase.Textual}`; check the scitype with
+  `schema(X)`
+
+Train the machine using `fit!(mach, rows=...)`.
+
+
+# Hyper-parameters
+
+
+- `max_doc_freq=1.0`: Restricts the vocabulary that the transformer will consider.
+  Terms that occur in `> max_doc_freq` documents will not be considered by the
+  transformer. For example, if `max_doc_freq` is set to 0.9, terms that are in more than
+  90% of the documents will be removed.
+- `min_doc_freq=0.0`: Restricts the vocabulary that the transformer will consider.
+  Terms that occur in `< max_doc_freq` documents will not be considered by the
+  transformer. A value of 0.01 means that only terms that are at least in 1% of the
+  documents will be included.
+- `smooth_idf=true`: Assuming `smooth_idf` is false, IDF is calculated using the equation
+  `idf(t) = log [ n / df(t) ] + 1`, with term `d`, `n` documents, and document frequency
+  `df(t)`. The `1` term outside of the logarithm has the effect that terms with zero idf
+  (i.e. they occur in all documents) will not be entirely ignored. If `smooth_idf` is
+  true, another `1` term is added, giving: `idf(t) = log [ (1 + n) / (1 + df(t)) ] + 1`.
+  These `1`'s have the same affect as adding an extra document which contains every term
+  in the collection exactly once, preventing division by 0.
+
+
+# Operations
+
+
+- `transform(mach, Xnew)`: Return a transformed matrix of scitype
+  `Continuous` given new features `Xnew`.
+
+
+# Fitted parameters
+
+
+The fields of `fitted_params(mach)` are:
+
+- `vocab`: A vector containing the string used in the transformer's vocabulary.
+- `idf_vector`: The transformer's calculated IDF vector.
+
+
+# Examples
+
+
+`TfidfTransformer` accepts a variety of inputs. In the example below, we use simple
+tokenized documents:
+
+```julia
+using MLJ, MLJText, TextAnalysis
+
+docs = ["Hi my name is Sam.", "How are you today?"]
+tfidf_transformer = TfidfTransformer()
+mach = machine(tfidf_transformer, tokenize.(docs))
+MLJ.fit!(mach)
+
+fitted_params(mach)
+
+tfidf_mat = transform(mach, tokenize.(docs))
+```
+
+We can also use the `TextAnalysis` package to implement funcionality similar to SciKit
+Learn's N-grams:
+
+```julia
+using MLJ, MLJText, TextAnalysis
+
+docs = ["Hi my name is Sam.", "How are you today?"]
+corpus = Corpus(NGramDocument.(docs, 1, 2))
+ngram_docs = ngrams.(corpus)
+
+tfidf_transformer = TfidfTransformer()
+mach = machine(tfidf_transformer, ngram_docs)
+MLJ.fit!(mach)
+fitted_params(mach)
+
+tfidf_mat = transform(mach, ngram_docs)
+```
+
+See also
+[`GaussianNBClassifier`](@ref)
+
 """
 mutable struct TfidfTransformer <: AbstractTextTransformer
     max_doc_freq::Float64
@@ -48,7 +113,7 @@ mutable struct TfidfTransformer <: AbstractTextTransformer
     smooth_idf::Bool
 end
 
-function TfidfTransformer(; max_doc_freq::Float64 = 1.0, min_doc_freq::Float64 = 0.0, smooth_idf::Bool = true)    
+function TfidfTransformer(; max_doc_freq::Float64 = 1.0, min_doc_freq::Float64 = 0.0, smooth_idf::Bool = true)
     transformer = TfidfTransformer(max_doc_freq, min_doc_freq, smooth_idf)
     message = MMI.clean!(transformer)
     isempty(message) || @warn message
@@ -60,7 +125,7 @@ struct TfidfTransformerResult
     idf_vector::Vector{Float64}
 end
 
-get_result(::TfidfTransformer, idf::Vector{<:AbstractFloat}, vocab::Vector{String}, ::SparseMatrixCSC) = 
+get_result(::TfidfTransformer, idf::Vector{<:AbstractFloat}, vocab::Vector{String}, ::SparseMatrixCSC) =
     TfidfTransformerResult(vocab, idf)
 
 function build_tfidf!(doc_term_mat::SparseMatrixCSC{T},
@@ -87,14 +152,14 @@ function build_tfidf!(doc_term_mat::SparseMatrixCSC{T},
     return tfidf
 end
 
-function _transform(::TfidfTransformer, 
+function _transform(::TfidfTransformer,
                     result::TfidfTransformerResult,
                     v::Corpus)
     doc_terms = build_dtm(v, result.vocab)
     tfidf = similar(doc_terms.dtm, eltype(result.idf_vector))
     build_tfidf!(doc_terms.dtm, tfidf, result.idf_vector)
 
-    # here we return the `adjoint` of our sparse matrix to conform to 
+    # here we return the `adjoint` of our sparse matrix to conform to
     # the `n x p` dimensions throughout MLJ
     return adjoint(tfidf)
 end
